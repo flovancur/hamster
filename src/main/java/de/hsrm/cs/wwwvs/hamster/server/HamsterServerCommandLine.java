d6fde84a6ca7f1adb51984e8548b198326c297b8
Florian Vancura <florian_vancura@gmx.de>
2023-05-05 16:54:22 +0200

    Namen extrahiert aus der Message

--- src/main/java/de/hsrm/cs/wwwvs/hamster/server/HamsterServerCommandLine.java
+++ src/main/java/de/hsrm/cs/wwwvs/hamster/server/HamsterServerCommandLine.java
@@ -4,7 +4,6 @@ import de.hsrm.cs.wwwvs.hamster.lib.HamsterException;
 import de.hsrm.cs.wwwvs.hamster.lib.HamsterLib;
 import de.hsrm.cs.wwwvs.hamster.lib.HamsterState;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Inet4Address;
@@ -28,6 +27,43 @@ public class HamsterServerCommandLine {
 		System.out.println("     -h {<IP address>}	- IP address to run the server on (default: 127.0.0.1)");
 		return 2;
 	}
+
+	private static void connect(){
+
+	}
+
+	private static byte[] charArrayToByteArray(char[] c_array) {
+		byte[] b_array = new byte[c_array.length];
+		for(int i= 0; i < c_array.length; i++) {
+			b_array[i] = (byte)(0xFF & (int)c_array[i]);
+		}
+		return b_array;
+	}
+
+	private static String generateOwnerName(char[] bufferOwner){
+		boolean end = false;
+		int i = 0;
+		while(!end && i<40){
+			if(bufferOwner[i+8]=='\u0000'){
+				end=true;
+			}
+			i++;
+		}
+		return new String(bufferOwner,8,4);
+	}
+
+	private static String generateHamsterName(char[] bufferOwner){
+		boolean end = false;
+		int i = 0;
+		while(!end && i<40){
+			if(bufferOwner[i+40]=='\u0000'){
+				end=true;
+			}
+			i++;
+		}
+		return new String(bufferOwner,40,4);
+	}
+
 
 	/**
 	 * The main command-line interface,
@@ -63,14 +99,17 @@ public class HamsterServerCommandLine {
 				Socket socket = serverSocket.accept();
 				InputStream input = socket.getInputStream();
 				InputStreamReader reader = new InputStreamReader(input);
-//				BufferedReader bufReader = new BufferedReader(reader);
 				char[] buffer = new char[socket.getReceiveBufferSize()];
-				int character = reader.read(buffer);
-				String output = new String(buffer,0,character);
-//				String output = bufReader.readLine();
+				int size = reader.read(buffer);
+				String output = new String(buffer);
+				byte[] bytes = charArrayToByteArray(buffer);
 
-				System.out.println(output);
-				System.out.println(output.length());
+				System.out.println(generateOwnerName(buffer));
+				System.out.println(generateHamsterName(buffer));
+
+				socket.close();
+
+
 			}
 			//System.exit(printRtfm());
 		} catch (Exception ex) {

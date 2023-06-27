0d9593de192d7f81fd949837cd8d716b3de786b9
Daniel Sandbühler <daniel.sandbuehler@student.hs-rm.de>
2023-06-27 20:12:52 +0200

    Http funktioniert soweit; hamster lib Implementierungen stehen zum Teil noch aus

--- src/main/java/de/hsrm/cs/wwwvs/hamster/server/HamsterController.java
+++ src/main/java/de/hsrm/cs/wwwvs/hamster/server/HamsterController.java
@@ -1,6 +1,8 @@
 package de.hsrm.cs.wwwvs.hamster.server;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -14,20 +16,29 @@ public class HamsterController {
 
     private static List<Hamster> hamsters = new ArrayList<>();
 
+
     @GetMapping("/hamster")
     public List<Hamster> getAllHamsters(@RequestParam(value = "name", required = false) String name) throws Exception {
         return hamsters;
     }
 
     @PostMapping("/hamster")
-    public ResponseEntity<String> addHamster(@RequestBody HamsterAddRequest request) throws Exception {
-        var hamster = new Hamster(request.name(), "TODO: insert owner", request.treats(), 17);
+    public ResponseEntity<String> addHamster(@RequestBody HamsterAddRequest request, @AuthenticationPrincipal Jwt token) throws Exception {
+        if(token != null) {
+            System.out.println("Claims: " + token.getClaims());
+        }
+        var hamster = new Hamster(request.name(),token.getClaims().get("given_name").toString(), request.treats(), 17);
         hamsters.add(hamster);
         return ResponseEntity.created(new URI("http://localhost:4200/hamster")).build();
     }
 
     @PostMapping("/hamster/{name}")
     public ResponseEntity<TreatsInfo> feed(@PathVariable String name, @RequestBody TreatsInfo treats) throws Exception {
+        System.out.println("post mapping /hamster/name triggered");
+        for(int i = 0; i < hamsters.size(); i++){
+            Hamster hamster = hamsters.get(i);
+            if (hamsters.get(i).name.equals(name)) hamsters.set(i, new Hamster(hamster.name(), hamster.owner(), hamster.treatsLeft() - treats.treats, hamster.cost()));
+        }
         return ResponseEntity.ok(new TreatsInfo((short)1));
     }
 
--- src/main/java/de/hsrm/cs/wwwvs/hamster/server/Security.java
+++ src/main/java/de/hsrm/cs/wwwvs/hamster/server/Security.java
@@ -3,12 +3,25 @@ package de.hsrm.cs.wwwvs.hamster.server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class Security {
     @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://kc.sls.cs.hs-rm.de/realms/SLS/protocol/openid-connect/certs").build();
+    }
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.build();
+        return http.authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/hamster").authenticated();
+                    authorize.anyRequest().permitAll();
+        })
+             .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder()))
+            )
+            .build();
     }
 }
